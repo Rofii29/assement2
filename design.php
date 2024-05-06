3edcc3b601a91f8b9d6efc9f269048eac0ef39e4
Rofii29 <cibihijau@gmail.com>
2024-05-06 12:43:30 +0700

    first commit

---
+++ design.php
@@ -0,0 +1,42 @@
+<?php
+
+// Koneksi ke database
+$servername = "localhost";
+$username = "root"; // Ganti dengan username Anda
+$password = ""; // Ganti dengan password Anda
+$database = "design"; // Ganti dengan nama database Anda
+
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Periksa koneksi
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
+}
+
+// Query untuk mengambil data dari tabel
+$sql = "SELECT * FROM nama";
+$result = $conn->query($sql);
+
+if ($result) {
+    $response = array();
+    while ($row = $result->fetch_assoc()) {
+        $response[] = $row;
+    }
+    
+    // Mengubah array menjadi format JSON
+    $json_data = json_encode($response);
+
+    // Menyimpan data JSON ke dalam file data.json
+    file_put_contents('data.json', $json_data);
+
+    echo "$json_data;";
+
+} else {
+    // Jika query gagal dijalankan
+    echo "Gagal menjalankan query: " . $conn->error;
+}
+
+// Tutup koneksi
+$conn->close();
+
+?>
